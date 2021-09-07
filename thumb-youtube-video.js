84609fdd3511417fc8543e4e90ba5dfb3301d374
Kayo Silva <kayo.silva@grancursosonline.com.br>
2021-09-07 10:18:34 -0300

    Ajuste na função

--- thumb-youtube-video.js
+++ thumb-youtube-video.js
@@ -1,7 +1,12 @@
-const getUrl = (codYoutube) => {
+function getUrl(codYoutube) {
     console.log('input -> ', codYoutube);
     return `https://i.ytimg.com/vi/${codYoutube.split('&')[0]}/maxresdefault.jpg`;
-};
+}
 
 console.log('1 output -> ', getUrl('rVX8z5iiKSw&list=PL9lh4wvvgovy16CSQhHnpM7Pi_KmGUY4V&index=1'));
 console.log('2 output -> ', getUrl('rVX8z5iiKSw'));
+
+// input ->  rVX8z5iiKSw&list=PL9lh4wvvgovy16CSQhHnpM7Pi_KmGUY4V&index=1
+// 1 output ->  https://i.ytimg.com/vi/rVX8z5iiKSw/maxresdefault.jpg
+// input ->  rVX8z5iiKSw
+// 2 output ->  https://i.ytimg.com/vi/rVX8z5iiKSw/maxresdefault.jpg
